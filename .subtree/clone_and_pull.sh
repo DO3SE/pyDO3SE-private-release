8de2c9ddcec13d4b288ca1b5ee75f7e66a3edc20
sam bland <sbland.co.uk@gmail.com>
2024-06-04 11:54:56 +0100

    Update scripts

--- .subtree/clone_and_pull.sh
+++ .subtree/clone_and_pull.sh
@@ -9,23 +9,27 @@ THERMAL_TIME_REMOTE="git@github.com:SEI-DO3SE/thermal_time"
 DO3SE_PHENOLOGY_REMOTE="git@github.com:SEI-DO3SE/do3se_phenology"
 DO3SE_MET_REMOTE="git@github.com:SEI-DO3SE/do3se_met"
 
+echo $1
+echo $2
+
+
 BRANCH=$2
 # TODO: Might need to use release branch
 # TODO: Fix if statement
-if [$1 = "clone"]; then
+if [[ "$1" == "clone" ]]; then
   git subtree add --prefix $PYDO3SE_LOCAL_PATH $PYDO3SE_REMOTE $BRANCH --squash
   git subtree add --prefix $THERMAL_TIME_LOCAL_PATH $THERMAL_TIME_REMOTE $BRANCH --squash
   git subtree add --prefix $DO3SE_PHENOLOGY_LOCAL_PATH $DO3SE_PHENOLOGY_REMOTE $BRANCH --squash
   git subtree add --prefix $DO3SE_MET_LOCAL_PATH $DO3SE_MET_REMOTE $BRANCH --squash
 fi
-if [$1 == "pull"]; then
+if [[ "$1" == "pull" ]]; then
   # Update subtree
   git subtree pull --prefix $PYDO3SE_LOCAL_PATH $PYDO3SE_REMOTE $BRANCH --squash
   git subtree pull --prefix $THERMAL_TIME_LOCAL_PATH $THERMAL_TIME_REMOTE $BRANCH --squash
   git subtree pull --prefix $DO3SE_PHENOLOGY_LOCAL_PATH $DO3SE_PHENOLOGY_REMOTE $BRANCH --squash
   git subtree pull --prefix $DO3SE_MET_LOCAL_PATH $DO3SE_MET_REMOTE $BRANCH --squash
 fi
-if [$1 == "tag"]; then
+if [[ "$1" == "tag" ]]; then
   # TODO: Complete tag implementation
   echo "Tagging"
   echo "Not implemented yet"
