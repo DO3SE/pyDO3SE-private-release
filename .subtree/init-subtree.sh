b8a37d5c389b94f890ca32e63210dfa6ddcce321
sam bland <sbland.co.uk@gmail.com>
2024-06-04 10:44:27 +0100

    update scripts

--- .subtree/init-subtree.sh
+++ .subtree/init-subtree.sh
@@ -5,12 +5,12 @@ DO3SE_PHENOLOGY_LOCAL_PATH="src/do3se_phenology"
 DO3SE_MET_LOCAL_PATH="src/do3se_met"
 
 # First check if repo is added
-printf "====Attempting to remove old copies of the repositories"
-git remote remove pyDO3SE
-git remote remove thermal_time
-git remote remove do3se_phenology
-git remote remove do3se_met
-printf "Removed old copies of the repositories=== \n\n"
+# printf "====Attempting to remove old copies of the repository links"
+# git remote remove pyDO3SE
+# git remote remove thermal_time
+# git remote remove do3se_phenology
+# git remote remove do3se_met
+# printf "Removed old copies of the repositories=== \n\n"
 
 
 # printf "====Attempting to add new copies of the repositories"
@@ -22,6 +22,12 @@ printf "Removed old copies of the repositories=== \n\n"
 
 
 PYDO3SE_REMOTE="git@github.com:SEI-DO3SE/pyDO3SE"
+THERMAL_TIME_REMOTE="git@github.com:SEI-DO3SE/thermal_time"
+DO3SE_PHENOLOGY_REMOTE="git@github.com:SEI-DO3SE/do3se_phenology"
+DO3SE_MET_REMOTE="git@github.com:SEI-DO3SE/do3se_met"
 
 
-git subtree add --prefix $PYDO3SE_LOCAL_PATH $PYDO3SE_REMOTE RELEASE --squash
+# git subtree add --prefix $PYDO3SE_LOCAL_PATH $PYDO3SE_REMOTE RELEASE --squash
+git subtree add --prefix $THERMAL_TIME_LOCAL_PATH $THERMAL_TIME_REMOTE RELEASE --squash
+# git subtree add --prefix $DO3SE_PHENOLOGY_LOCAL_PATH $DO3SE_PHENOLOGY_REMOTE RELEASE --squash
+# git subtree add --prefix $DO3SE_MET_LOCAL_PATH $DO3SE_MET_REMOTE RELEASE --squash
