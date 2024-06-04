31e880c196cc46620015b97015180f1bc42d2f34
sam bland <sbland.co.uk@gmail.com>
2024-06-04 11:50:50 +0100

    Update scripts and requirements

--- .subtree/clone_and_pull.sh
+++ .subtree/clone_and_pull.sh
@@ -4,33 +4,33 @@ THERMAL_TIME_LOCAL_PATH="src/thermal_time"
 DO3SE_PHENOLOGY_LOCAL_PATH="src/do3se_phenology"
 DO3SE_MET_LOCAL_PATH="src/do3se_met"
 
-# First check if repo is added
-git remote remove pyDO3SE
-git remote remove thermal_time
-git remote remove do3se_phenology
-git remote remove do3se_met
+PYDO3SE_REMOTE="git@github.com:SEI-DO3SE/pyDO3SE"
+THERMAL_TIME_REMOTE="git@github.com:SEI-DO3SE/thermal_time"
+DO3SE_PHENOLOGY_REMOTE="git@github.com:SEI-DO3SE/do3se_phenology"
+DO3SE_MET_REMOTE="git@github.com:SEI-DO3SE/do3se_met"
 
-git remote add -f pyDO3SE git@github.com/SEI-DO3SE/pyDO3SE
-git remote add -f thermal_time git@github.com/SEI-DO3SE/thermal_time
-git remote add -f do3se_phenology git@github.com/SEI-DO3SE/do3se_phenology
-git remote add -f do3se_met git@github.com/SEI-DO3SE/do3se_met
-
+BRANCH=$2
 # TODO: Might need to use release branch
 # TODO: Fix if statement
 if [$1 = "clone"]; then
-  git subtree add --prefix $PYDO3SE_LOCAL_PATH pyDO3SE main --squash
-  git subtree add --prefix $THERMAL_TIME_LOCAL_PATH thermal_time main --squash
-  git subtree add --prefix $DO3SE_PHENOLOGY_LOCAL_PATH do3se_phenology main --squash
-  git subtree add --prefix $DO3SE_MET_LOCAL_PATH do3se_met main --squash
+  git subtree add --prefix $PYDO3SE_LOCAL_PATH $PYDO3SE_REMOTE $BRANCH --squash
+  git subtree add --prefix $THERMAL_TIME_LOCAL_PATH $THERMAL_TIME_REMOTE $BRANCH --squash
+  git subtree add --prefix $DO3SE_PHENOLOGY_LOCAL_PATH $DO3SE_PHENOLOGY_REMOTE $BRANCH --squash
+  git subtree add --prefix $DO3SE_MET_LOCAL_PATH $DO3SE_MET_REMOTE $BRANCH --squash
 fi
 if [$1 == "pull"]; then
   # Update subtree
-  git subtree pull --prefix $PYDO3SE_LOCAL_PATH pyDO3SE main --squash
-  git subtree pull --prefix $THERMAL_TIME_LOCAL_PATH thermal_time main --squash
-  git subtree pull --prefix $DO3SE_PHENOLOGY_LOCAL_PATH do3se_phenology main --squash
-  git subtree pull --prefix $DO3SE_MET_LOCAL_PATH do3se_met main --squash
+  git subtree pull --prefix $PYDO3SE_LOCAL_PATH $PYDO3SE_REMOTE $BRANCH --squash
+  git subtree pull --prefix $THERMAL_TIME_LOCAL_PATH $THERMAL_TIME_REMOTE $BRANCH --squash
+  git subtree pull --prefix $DO3SE_PHENOLOGY_LOCAL_PATH $DO3SE_PHENOLOGY_REMOTE $BRANCH --squash
+  git subtree pull --prefix $DO3SE_MET_LOCAL_PATH $DO3SE_MET_REMOTE $BRANCH --squash
 fi
 if [$1 == "tag"]; then
+  # TODO: Complete tag implementation
+  echo "Tagging"
+  echo "Not implemented yet"
+fi
+
 # TODO: Update version info
 # Should store the open version somewhere
 
