dce6238f3245b1522092a3fe63bcf4a26f8d1070
sam bland <sbland.co.uk@gmail.com>
2024-11-19 11:23:01 +0000

    Update env setup

--- env.init.sh
+++ env.init.sh
@@ -3,3 +3,10 @@ source .venv/bin/activate
 uv pip install -r requirements/common.txt
 uv pip install -r requirements/tests.txt
 uv pip install -r requirements/local.txt --no-deps # These should be installed separately to ensure they do not clash with the other requirements
+
+# Copy integration tests from pyDO3SE
+rm -R tests/key_processes
+cp -R src/pyDO3SE/tests/key_processes tests/key_processes
+
+rm -R examples
+cp -R src/pyDO3SE/examples examples
