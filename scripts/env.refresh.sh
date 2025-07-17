75b593c0c25bb6a0a03293fc81e2cb2416cb1eb7
Sam Bland <sam.bland@york.ac.uk>
2025-07-17 23:47:36 +0100

    Fix test util copy

--- scripts/env.refresh.sh
+++ scripts/env.refresh.sh
@@ -8,7 +8,9 @@ uv sync --all-groups
 # Copy integration tests from pyDO3SE
 rm -R tests/key_processes
 mkdir -p tests/key_processes
+touch tests/__init__.py
 cp -R src/pyDO3SE/tests/key_processes tests
+cp src/pyDO3SE/tests/utils.py tests/utils.py
 
 rm -R examples
 mkdir -p examples
