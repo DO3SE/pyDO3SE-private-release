6939832dce0e9734b61ef60331db0ee2521838e2
Sam Bland <sam.bland@york.ac.uk>
2025-11-28 20:01:18 +0000

    Squashed 'src/pyDO3SE/' changes from 62a495e3b5..a434e29ff4
    a434e29ff4 RELEASE
    REVERT: 62a495e3b5 RELEASE
    
    git-subtree-dir: src/pyDO3SE
    git-subtree-split: a434e29ff4859b500a620d3822b2ea3417af8fd5

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ copyright = '2020,(SEI)'
 author = '(SEI)'
 
 # The full version, including alpha/beta/rc tags
-release = '4.44.0'
+release = '4.44.2'
 
 
 # -- General configuration ---------------------------------------------------
