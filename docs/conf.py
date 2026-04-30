1436b1daf02494d62336982431e72062377fdd35
Sam Bland <sam.bland@york.ac.uk>
2026-04-30 21:00:35 +0100

    Squashed 'src/pyDO3SE/' changes from 41cf8cc5..2f06aab1
    2f06aab1 RELEASE
    REVERT: 41cf8cc5 RELEASE
    
    git-subtree-dir: src/pyDO3SE
    git-subtree-split: 2f06aab122ca86aa6e6c1eb142580e356ac038cf

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ copyright = '2020,(SEI)'
 author = '(SEI)'
 
 # The full version, including alpha/beta/rc tags
-release = '4.44.22'
+release = '4.44.23'
 
 
 # -- General configuration ---------------------------------------------------
--- pyDO3SE/version.py
+++ pyDO3SE/version.py
@@ -1,5 +1,5 @@
 """Current version."""
-version = '4.44.22'
+version = '4.44.23'
 config_version = 12
 
 if __name__ == "__main__":
